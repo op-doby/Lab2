7b6b60575cee13c8795b3adba88a73ab196f2003
“op-doby” <ophir7@gmail.com>
2024-06-04 13:22:23 +0300

    to delete thr comments

--- makefile
+++ makefile
@@ -1,13 +1,13 @@
 myshell: myshell.o LineParser.o 
 	gcc -g -m32 -Wall  -o myshell myshell.o LineParser.o 
-Looper: Looper.o
-	gcc -g -m32 -Wall -o Looper Looper.o
+looper: Looper.o
+	gcc -g -m32 -Wall -o looper Looper.o
 myshell.o: myshell.c LineParser.h
 	gcc -m32 -g -Wall -c -o myshell.o myshell.c 
 LineParser.o: LineParser.c LineParser.h
 	gcc -m32 -g -Wall -c -o LineParser.o LineParser.c 
-Looper.o: Looper.c 
-	gcc -m32 -g -Wall -c -o Looper.o Looper.c 
+looper.o: Looper.c 
+	gcc -m32 -g -Wall -c -o looper.o Looper.c 
 .PHONY: clean 
 clean: 
 	rm -f *.o myshell
--- myshell.c
+++ myshell.c
@@ -1,13 +1,13 @@
-#include <linux/limits.h>
 #include <stdio.h>
-#include "LineParser.h"
-#include <sched.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
-#include <asm-generic/signal.h>
-
+#include <sys/wait.h>
+#include <limits.h>
+#include "LineParser.h"
+#include <fcntl.h>
+#include <asm-generic/fcntl.h>
 
 //ps - report a snapshot of the current processes.
 //int kill(pid_t pid, int sig);
@@ -21,40 +21,73 @@
        //except for process 1 (init), but see below.
 
 void execute(cmdLine *pCmdLine) {
+
     if (strcmp((*pCmdLine).arguments[0], "cd") == 0) { 
-        if (chdir((*pCmdLine).arguments[1]) == -1) { 
+        if (chdir((*pCmdLine).arguments[1]) == -1) {  //arguments[1] is the path to the directory
             fprintf(stderr, "cd: %s: No such file or directory\n", (*pCmdLine).arguments[1]); 
         }
     }
-    //TO DO: delete and add the rest of the commands
     //Arguments[0] is the command name
-    pid_t pid = atoi((*pCmdLine).arguments[1]); // Get the process ID from the argument
      else if (strcmp((*pCmdLine).arguments[0], "alarm") == 0) { // Check if the command is "alarm"
-        //pid_t pid = atoi((*pCmdLine).arguments[1]); 
+        pid_t pid = atoi((*pCmdLine).arguments[1]); 
         if (kill(pid, SIGCONT) == 0) { // Send SIGCONT signal to wake up the process
             printf("Process %d woke up\n", pid);
         } else {
             perror("ERROR: kill()"); 
         }
     } else if (strcmp((*pCmdLine).arguments[0], "blast") == 0) { // Check if the command is "blast"
-        //pid_t pid = atoi((*pCmdLine).arguments[1]); 
+        pid_t pid = atoi((*pCmdLine).arguments[1]); 
         if (kill(pid, SIGKILL) == 0) { // Send SIGKILL signal to terminate the process
             printf("Process %d terminated\n", pid);
         } else {
-            perror("ERROR: kill()"); 
+            perror("ERROR: kill() failed\n"); 
         } 
     }
+    ///////quit ????? in the main 
     else {
         pid_t p = fork();
         if(p ==-1){
-            perror("fork() error");
+            perror("ERROR: fork() failed\n");
             exit(1);
         }
         if (p == 0) {
+            if((*pCmdLine).inputRedirect != NULL){
+                int input_file = open((*pCmdLine).inputRedirect, O_RDONLY);
+            if (input_file == -1) {
+                perror("ERROR: couldn't open the input file for reading");
+                _exit(1);
+            }
+            // Duplicate the file descriptor to stdin (file descriptor 0)
+            int result = dup2(input_file, STDIN_FILENO);
+            if (result == -1) {
+                perror("ERROR: couldn't accomplish dup2() for input redirection");
+                _exit(1);
+            }
+            close(input_file);
+            }
+            if((*pCmdLine).outputRedirect != NULL){
+                int output_file = open((*pCmdLine).outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (output_file == -1) {
+                    perror("ERROR: couldn't open the output file for writing");
+                    _exit(1);
+                }
+                // Duplicate the file descriptor to stdout (file descriptor 1)
+                int result = dup2(output_file, STDOUT_FILENO);
+                if (result == -1) {
+                    perror("ERROR: couldn't accomplish dup2() for output redirection");
+                    _exit(1);
+                }
+                result = dup2(output_file, STDERR_FILENO); // Redirect stderr to the same file descriptor as stdout
+                if (result == -1) {
+                    perror("ERROR: couldn't accomplish dup2() for error redirection");
+                    _exit(1);
+                }
+                close(output_file);
+                }
             fprintf(stderr, "PID: %d\n", getpid());
             fprintf(stderr, "Executing command: %s\n", (*pCmdLine).arguments[0]);
             execvp((*pCmdLine).arguments[0], (*pCmdLine).arguments);
-            perror("execvp() error"); // If execvp returns, it means an error occurred, and the current process was not replaced. 
+            perror("ERROR: execvp() failed\n"); // If execvp returns, it means an error occurred, and the current process was not replaced. 
             _exit(1); //Used in situations where the process needs to terminate immediately without 
             //performing any cleanup, such as in the child process after a fork where performing such cleanup could be 
             //problematic or redundant.
@@ -83,7 +116,7 @@ int main(){
             fflush(stdout);
         }
         else{
-            perror("getcwd() error"); 
+            perror("ERROR: getcwd() failed\n"); 
             return 1; 
         }
         if(fgets(input, 2048, stdin)!=NULL){ // read input from the user
@@ -96,14 +129,14 @@ int main(){
             }
             parsedLine = parseCmdLines(input);
             if(parsedLine == NULL){
-                perror("parseCmdLines() error");
+                perror("ERROR: parseCmdLines() failed\n");
                 continue;
             }
             execute(parsedLine);
             freeCmdLines(parsedLine);
         }
         else{
-            perror("fgets() error");
+            perror("ERROR: fgets() failed\n");
             printf("\n");
             return 1; 
         }    
